fc7d4fb88b08a5c1840e4ae2b18ac8539c3d3eaf
ChamodiBandara <chamodidevini@gmail.com>
2025-09-04 02:01:49 +0530

    Fix errors in Event API and update configs

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose'); //import mongoose ,the library that connects Node.js to MongoDB
 
-const connectDB = async () => {  //function that connect to MongoDB
+const connectDB = async () => { //function that connect to MongoDB
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI); //Read Url and connect backend to MongoDB Atlas asynchronously
+    await mongoose.connect(process.env.MONGO_URI); // FIXED: Removed useless 'conn' assignment
     console.log(`MongoDB connected`); //print the host of the mongodb cluster if connected successfully 
   } catch (err) {
     console.error(err.message); // if the connection fail prints error
